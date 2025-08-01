f12cdc4c1f9c251fb671993f26ac5fd19419f395
KateLIU55 <liadam364@gmail.com>
2025-07-31 20:20:17 -0700

    Create studentEnrollSystemTest file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,11 @@ insert into term (term_id, tyear, semester, add_date, add_deadline, drop_deadlin
 insert into user_table (id, name, email, password, type) values
 (1, 'admin', 'admin@csumb.edu', '$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW' , 'ADMIN'),
 (2, 'sam', 'sam@csumb.edu', '$2a$10$B3E9IWa9fCy1SaMzfg1czu312d0xRAk1OU2sw5WOE7hs.SsLqGE9O', 'STUDENT'),
-(3, 'ted', 'ted@csumb.edu', '$2a$10$YU83ETxvPriw/t2Kd2wO8u8LoKRtl9auX2MsUAtNIIQuKROBvltdy', 'INSTRUCTOR');
+(3, 'ted', 'ted@csumb.edu', '$2a$10$YU83ETxvPriw/t2Kd2wO8u8LoKRtl9auX2MsUAtNIIQuKROBvltdy', 'INSTRUCTOR'),
+(4, 'sally', 'sally@csumb.edu', '$2a$10$LbnNBkctBxuHlZDdaKMW6OPIFkPHvKJ/8WaT4ZtA6KdR/ANm/dA0O', 'INSTRUCTOR'),
+(5, 'bob', 'bob@csumb.edu', '$2a$10$/CdPw/L6tWg0bNewmIFgmO2k2sZpVbYFYzvLZeT5b2o.xX2Pt78RK', 'STUDENT');
+
+
 
 
 insert into course values
@@ -13,11 +17,34 @@ insert into course values
 ('cst334', 'Operating Systems', 4),
 ('cst363', 'Introduction to Database', 4),
 ('cst489', 'Software Engineering', 4),
-('cst499', 'Capstone', 4);
+('cst499', 'Capstone', 4),
+('cst393', 'Software Engineering Project', 4),
+('cst328', 'Digital art and design', 4),
+('cst370', 'Design and analysis algorithms', 4),
+('cst383', 'Introduction to data science', 4);
+
+
+
 
 insert into section (section_no, course_id, section_id, term_id, building, room, times, instructor_email) values
 (1, 'cst489', 1, 10, '90', 'B104', 'W F 10-11', 'ted@csumb.edu');
 
+-- add open sections for Fall 2025
+insert into section (section_no, course_id, section_id, term_id, building, room, times, instructor_email) values
+(2, 'cst336',  2, 10, '90',  'B105', 'M W 9-10',  'ted@csumb.edu'),
+(3, 'cst499',  3, 10, '90',  'B106', 'T Th 11-12','sally@csumb.edu'),
+(10, 'cst393', 10, 10, '92', 'B107', 'M W 2-4', 'sally@csumb.edu'),
+(11, 'cst370', 11, 10, '95', 'B110', 'F 8-10', 'ted@csumb.edu');
+
+
+-- add some open sections for Spring 2025 too
+insert into section (section_no, course_id, section_id, term_id, building, room, times, instructor_email) values
+(4, 'cst334',  4, 9, '01',  '201', 'T Th 1-2',   'ted@csumb.edu'),
+(5, 'cst363',  5, 9, '01',  '202', 'M W 10-11', 'ted@csumb.edu'),
+(6, 'cst328',  6, 9, '01',  '203', 'T F 6-9', 'ted@csumb.edu'),
+(7, 'cst370',  7, 9, '01',  '205', 'M TH 1-4', 'sally@csumb.edu'),
+(8, 'cst383',  8, 9, '01',  '208', 'W F 9-11', 'sally@csumb.edu'),
+(9, 'cst393',  9, 9, '01',  '209', 'T TH 2-6', 'sally@csumb.edu');
 
 
 
